142136d9cfa6961c548bb81ee4d62f49d83b6c41
Sanches <sanches-zotov@yandex.ru>
2020-11-07 15:17:56 +0300

    feat: сделать добавление карточек через js

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,7 @@ let profile = document.querySelector('.profile');
 let editButton = profile.querySelector('.button_type_edit');
 let title = profile.querySelector('.profile__title');
 let subtitle = profile.querySelector('.profile__subtitle');
+let elements = document.querySelector('.elements__list');
 
 
 function popupOpen() {
@@ -34,6 +35,53 @@ function formSubmitHandler(evt) {
 editButton.addEventListener('click', popupOpen);
 closeButton.addEventListener('click', popupClose);
 popupForm.addEventListener('submit', formSubmitHandler);
+
+const initialCards = [
+    {
+        name: 'Барселона',
+        link: './images/Barselona.jpg',
+        alt: 'картинка: Барселона'
+    },
+    {
+        name: 'Порту',
+        link: './images/Porto.jpg',
+        alt: 'картинка: Порту'
+    },
+    {
+        name: 'Lez Arc Франция',
+        link: './images/Lez-Ark.jpg',
+        alt: 'картинка: Lez Arc Франция'
+    },
+    {
+        name: 'Шри-Ланка',
+        link: './images/Shri-Lanka.jpg',
+        alt: 'картинка: Шри-Ланка'
+    },
+    {
+        name: 'Бали',
+        link: './images/Bali.jpg',
+        alt: 'картинка: Бали'
+    },
+    {
+        name: 'Копенгаген',
+        link: './images/Copenhagen.JPG',
+        alt: 'картинка: Копенгаген'
+    }
+];
+
+
+initialCards.forEach(function (item) {
+
+    const cardTemplate = document.querySelector('#card').content;
+    const cardElement = cardTemplate.cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = item.link;
+    cardElement.querySelector('.card__title').textContent = item.name;
+    cardElement.querySelector('.card__image').alt = item.alt;
+
+    elements.append(cardElement);
+
+});
 
 
 
