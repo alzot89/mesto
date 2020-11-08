fe9ff41ada09ccf5b647bb023a7bdadcc9327939
Sanches <sanches-zotov@yandex.ru>
2020-11-08 13:44:47 +0300

    feat: добавть второй попап

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,53 @@
-let popup = document.querySelector('.popup');
-let popupForm = popup.querySelector('.popup__form');
-let closeButton = popup.querySelector('.button_type_close');
+let popupEditType = document.querySelector('.popup_type_edit');
+let popupAddType = document.querySelector('.popup_type_add');
+let popupForm = popupEditType.querySelector('.popup__form');
+let closeButton = popupEditType.querySelector('.button_type_close');
+let secondPopupForm = popupAddType.querySelector('.popup__form');
+let secondCloseButton = popupAddType.querySelector('.button_type_close');
+
 let name = popupForm.querySelector('.popup__input_type_name');
 let job = popupForm.querySelector('.popup__input_type_job');
+
+let image = secondPopupForm.querySelector('.popup__input_type_image');
+let link = secondPopupForm.querySelector('.popup__input_type_link');
+
 let profile = document.querySelector('.profile');
 let editButton = profile.querySelector('.button_type_edit');
+let addButton = profile.querySelector('.button_type_add');
 let title = profile.querySelector('.profile__title');
 let subtitle = profile.querySelector('.profile__subtitle');
 let elements = document.querySelector('.elements__list');
 
-
-function popupOpen() {
+editButton.addEventListener('click', function () {
     name.value = title.textContent;
     job.value = subtitle.textContent;
-    popup.classList.add('popup_opened');
-}
+    popupEditType.classList.add('popup_opened');
+});
 
-function popupClose() {
-    popup.classList.remove('popup_opened');
-}
+addButton.addEventListener('click', function () {
+    popupAddType.classList.add('popup_opened');
+});
 
+closeButton.addEventListener('click', function () {
+    popupEditType.classList.remove('popup_opened');
+});
 
-function formSubmitHandler(evt) {
+secondCloseButton.addEventListener('click', function () {
+    popupAddType.classList.remove('popup_opened');
+})
+popupEditType.addEventListener('mousedown', function (evt) {
+    if (evt.target.classList.contains('popup')) {
+        popupEditType.classList.remove('popup_opened');
+    };
+});
+
+popupForm.addEventListener('submit', function (evt) {
     evt.preventDefault();
 
     title.textContent = name.value;
     subtitle.textContent = job.value;
-
-    popupClose();
-
-}
-
-
-editButton.addEventListener('click', popupOpen);
-closeButton.addEventListener('click', popupClose);
-popupForm.addEventListener('submit', formSubmitHandler);
+    popupEditType.classList.remove('popup_opened');
+});
 
 const initialCards = [
     {
@@ -84,6 +97,16 @@ initialCards.forEach(function (item) {
 });
 
 
+secondPopupForm.addEventListener('submit', function (evt) {
+    evt.preventDefault();
 
+    const cardTemplate = document.querySelector('#card').content;
+    const cardElement = cardTemplate.cloneNode(true);
 
+    cardElement.querySelector('.card__image').src = link.value;
+    cardElement.querySelector('.card__title').textContent = image.value;
+
+    elements.prepend(cardElement);
+    popupAddType.classList.remove('popup_opened');
+});
 
