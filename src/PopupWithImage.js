452954c23e78bb139a19c4047db6a62e438b2c07
Sanches <sanches-zotov@yandex.ru>
2020-12-25 13:00:42 +0300

    fix: передавать данные карточки в метод open

--- src/PopupWithImage.js
+++ src/PopupWithImage.js
@@ -1,19 +1,15 @@
 import Popup from './Popup.js';
 export default class PopupWithImage extends Popup {
-    constructor({ name, link, alt }, popupSelector) {
+    constructor(popupSelector) {
         super(popupSelector);
-        this._name = name;
-        this._link = link;
-        this._alt = alt;
     }
 
 
-    open() {
+    open({ image, link }) {
         const popupImage = this._popup.querySelector('.popup__image');
         const popupImageTitle = this._popup.querySelector('.popup__image-title');
-        popupImage.src = this._link;
-        popupImageTitle.textContent = this._name;
-        popupImage.alt = this._alt;
+        popupImage.src = link;
+        popupImageTitle.textContent = image;
         super.open();
     }
 }
