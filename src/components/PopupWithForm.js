1159c621c3497f6d9457cb504e34a78c2f51240b
Sanches <sanches-zotov@yandex.ru>
2020-12-26 20:17:18 +0300

    fix: удалить методы очистки форм

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,8 +5,8 @@ export default class PopupWithForm extends Popup {
         super(popupSelector);
         this._handleFormSubmit = handleFormSubmit;
         this._form = this._popup.querySelector('.popup__form');
-        this._inputList = this._form.querySelectorAll('.popup__input');
         this._submitHandler = this._submitHandler.bind(this);
+        this._inputList = this._form.querySelectorAll('.popup__input');
     }
 
     setEventListeners() {
@@ -20,12 +20,6 @@ export default class PopupWithForm extends Popup {
         this.close();
     }
 
-    open() {
-        super.open();
-        this._removeErrorMessages();
-        this._setButtonState(this._form.checkValidity());
-    }
-
     close() {
         super.close();
         this._form.reset();
@@ -35,27 +29,6 @@ export default class PopupWithForm extends Popup {
         this._formValues = {};
         this._inputList.forEach(input => this._formValues[input.name] = input.value);
         return this._formValues;
-    }
-
-    _setButtonState(isActive) {
-        const button = this._form.querySelector('.popup__save')
-        if (!isActive) {
-            button.classList.add('popup__save_invalid');
-            button.disabled = true;
-        } else {
-            button.classList.remove('popup__save_invalid');
-            button.disabled = false;
-        }
-    }
-
-    _removeErrorMessages() {
-        this._errorList = this._form.querySelectorAll('.error');
-        this._errorList.forEach((error) => {
-            error.textContent = " ";
-        });
-        this._inputList.forEach((input) => {
-            input.classList.remove('popup__input_state_invalid');
-        });
     }
 
     _removeEventListeners() {
